52bc77e54fa7143cadd7ce842891b0c91806a4c3
fergush <fergus.hewson@unlimitedrealities.com>
2015-06-09 13:12:22 +1200

    Save point

--- platforms/browser/www/js/dbhelp.js
+++ platforms/browser/www/js/dbhelp.js
@@ -7,7 +7,7 @@ function dbConnect(){
 }
 
 function populateDB(tx){
-	tx.executeSql('DROP TABLE counters');
+//	tx.executeSql('DROP TABLE counters');
 	tx.executeSql('CREATE TABLE IF NOT EXISTS settings (key TEXT PRIMARY KEY, value TEXT NOT NULL)');
 	tx.executeSql('CREATE TABLE IF NOT EXISTS counters (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, value INTEGER NOT NULL, increment INTEGER NOT NULL)');
        //	tx.executeSql('INSERT INTO counters(name,value) VALUES ("swears", 1)');
@@ -24,32 +24,26 @@ function getCounters(){
 }
 function renderCounters(tx, results){
 	console.log('RENDER COUNTERS');
-	var counterList = $('#counterList')
+	var counterList = $('#counterList');
 	counterList.empty();
+	console.log('TODO cleanup event handlers on li');
+	
 	$.each(results.rows,function(index){
 		var row = results.rows.item(index);
 	        
 		var html = '<li id="count-'+row['id']+'" class="counter-li">'; 
-//		html+='<div class="column-a"><input type="submit" class="delete-btn fa-input fa-3x" value="&#xf068;" onclick="decrementCounter('+row['id']+')"></input></div>';
- 		html+='<div class="column-a"><i class="fa fa-minus fa-3x inc-btn" onclick="decrementCounter('+row['id']+','+row['increment']+')"></i></div>'		
+ 		html+='<div id="dec-btn-'+row['id']+'" class="column-a"><i class="fa fa-minus fa-3x inc-btn" onclick="decrementCounter('+row['id']+','+row['increment']+')"></i></div>'		
 		html+='<div class="column-b">';
 		html+='<h4 class="counter-name">'+row['name']+'</h4>';
 		
-		html+='<h4 class="counter-value">'+row['value']+'</h4>';
+		html+='<h4 id="cv-'+row['id']+'" class="counter-value">'+row['value']+'</h4>';
 		html+='</div>'
-		//html+='<div class="column-c"><input type="submit" class="btn fa-input fa-3x" value="&#xf067;" onclick="incrementCounter('+row['id']+')"></input></div>';
-		html+='<div class="column-c"><i href="" class="fa fa-3x fa-plus inc-btn" onclick="incrementCounter('+row['id']+','+row['increment']+')"></i></div>'		
+		html+='<div id="inc-btn-'+row['id']+'" class="column-c"><i href="" class="fa fa-3x fa-plus inc-btn" onclick="incrementCounter('+row['id']+','+row['increment']+')"></i></div>'		
 	
-		//html+='<div class="column-d"><input type="submit" class="btn fa-input fa-3x" value="&#xf1f8;" onclick="removeCounter('+row['id']+')"></input></div>';
-		//html+='<div class="column-d"><i href="" class="fa fa-3x fa-trash d-btn" onclick="removeCounter('+row['id']+')"></i></div>'		
-	
-//		html='<input type="submit" class="btn fa-input" value="&#xf1f8;">'
-
 		html+='</li>';
 
 //		console.log(html)	
 		counterList.append(html);
-		//$( "#count-"+row['id'] ).on( "swipe", swipeHandler );		
 		
 		$("#count-"+row['id']).swiperight(function(){
 		//	console.log("SWIPE R" + row['id'] );
@@ -59,6 +53,8 @@ function renderCounters(tx, results){
 			console.log("SWIPE L" + row['id'] );
 			removeCounter(row['id']);
 		});
+
+
 	
 	}
 	);
@@ -98,36 +94,65 @@ function addCounter(){
 
 function removeCounter(id){
 	console.log('REMOVE COUNTER ' + id)
+	
+	// Fade out li
+	$("#count-"+id).stop(true,true).fadeOut("slow",function(){}); 
+	
+	
 	var db = window.openDatabase("counters", "1.0", "counters", 200000);
 	db.transaction(function(tx){
 		tx.executeSql("DELETE FROM counters WHERE id= "+id,[],successCB,errorCB)	
 	});
 	navigator.vibrate(500);
 	//redraw counters
-	getCounters();
+	//getCounters();
 }
 function decrementCounter(id, diff){
 	console.log('Decrement Counter');
 	var db = window.openDatabase("counters", "1.0", "counters", 200000);
+	
+	// Get and update value in html
+	elem = $("#cv-"+id);
+	var val = parseInt(elem.text());
+	val = val - diff;
+	elem.text(val);
+	// Update db
 	db.transaction(function(tx){
-		tx.executeSql("UPDATE counters SET value = value - "+diff+" WHERE id = "+id,[],successCB,errorCB);	
+		tx.executeSql("UPDATE counters SET value = "+val+" WHERE id = "+id,[],successCB,errorCB);	
 	});
 
-	//redraw counters
-	getCounters();
+	// wiggle dec btn
+	doBounce($("#dec-btn-"+id), 3, '10px', 50);
+	animateUpdate(id);
+	navigator.vibrate(500);
+	playClick();
 }
 function incrementCounter(id, diff){
 	console.log('Increment Counter');
 	var db = window.openDatabase("counters", "1.0", "counters", 200000);
+
+	elem = $("#cv-"+id);
+	var val = parseInt(elem.text());
+	val = val + diff;
+	elem.text(val);
+	
 	db.transaction(function(tx){
-		tx.executeSql("UPDATE counters SET value = value + "+diff+" WHERE id = "+id,[],successCB,errorCB);
+		tx.executeSql("UPDATE counters SET value = "+val+" WHERE id = "+id,[],successCB,errorCB);
 	});
+	
+	// wiggle inc btn
+	//$("#inc-btn-"+id).stop(true,true).effect('shake',{times:3,distance:5,direction:"up"},250);
+	doBounce($("#inc-btn-"+id), 3, '10px', 50);
+	
+	animateUpdate(id);
 	navigator.vibrate(500);
 	playClick();
-	//redraw counters
-	getCounters();
 }
-
+function animateUpdate(id){
+	console.log('AnimateUpdate')
+	$('#cv-'+id).stop(true,true).animate({opacity:0},250);
+	$('#cv-'+id).stop(true,true).animate({opacity:1},250);
+}
 function errorCB(err){
 	console.log("Error processing SQL : " + err.code );       
 }
@@ -164,4 +189,11 @@ function playClick(){
     	);
         // Play audio
     	myAudio.play();
+}
+
+function doBounce(element, times, distance, speed) {
+    for(i = 0; i < times; i++) {
+        element.animate({marginTop: '-='+distance},speed)
+            .animate({marginTop: '+='+distance},speed);
+    }        
 }
